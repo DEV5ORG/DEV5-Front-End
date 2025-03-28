ff0cc5286c009e443a3715b566d8c68c1b208bbb
dfiatt <diegofiatt2323@gmail.com>
2025-03-27 20:29:46 -0600

    Loading services from endpoint

--- services/services.service.ts
+++ services/services.service.ts
@@ -2,10 +2,10 @@ import api from "./axios-instance";
 
 export const getServices = async () => {
   try {
-    const { data } = await api.get("/api/servicios");
-    console.log(data)
+    const { data } = await api.get("/api/servicios/sin-items");
     return data;
   } catch (error) {
-    // Handle error
+    console.error("Error fetching services:", error);
+    throw error;
   }
 };
