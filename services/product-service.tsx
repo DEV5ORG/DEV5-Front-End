b272a6fa976cc90235ffef11a34610ac30563bce
Elizabeth <esequeiras@ucenfotec.ac.cr>
2025-03-31 22:20:55 -0600

    se implementó el endpoint para traer los productos de un servicio

--- services/product-service.tsx
+++ services/product-service.tsx
@@ -1,12 +1,12 @@
 import api from "./axios-instance";
 
-export const fetchProductsByService = async (serviceId: string) => {
+export const fetchServiceById = async (serviceId: string) => {
   try {
-    const { data } = await api.get(`/api/products?serviceId=${serviceId}`); 
+    const { data } = await api.get(`/api/servicios/${serviceId}`);
     return data;
   } catch (error) {
-    console.error("Error fetching products by service:", error);
-    throw new Error("No se pudieron obtener los productos.");
+    console.error("Error fetching service by ID:", error);
+    throw new Error("Error al cargar los datos .");
   }
 };
 
