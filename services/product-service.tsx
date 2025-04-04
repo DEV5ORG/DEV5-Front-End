d2f8a63ed94c5ef56d1c7cfc47ca8c080447e523
Elizabeth <esequeiras@ucenfotec.ac.cr>
2025-04-03 21:05:15 -0600

    remove searchProducts el filtro se hace desde el FE

--- services/product-service.tsx
+++ services/product-service.tsx
@@ -9,13 +9,3 @@ export const fetchServiceById = async (serviceId: string) => {
     throw new Error("Error al cargar los datos .");
   }
 };
-
-export const searchProducts = async (serviceId: string, query: string) => {
-  try {
-    const { data } = await api.get(`/api/products/search?serviceId=${serviceId}&query=${query}`);
-    return data;
-  } catch (error) {
-    console.error("Error searching products:", error);
-    throw new Error("No se pudieron buscar los productos.");
-  }
-};
